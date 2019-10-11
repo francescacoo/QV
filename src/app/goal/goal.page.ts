dd5e6188fcac97cb631a671cb73bb8aa5e9f8adc
francescacoo <fplacido@paypal.com>
2019-10-11 18:48:16 +0200

    goal

--- src/app/goal/goal.page.ts
+++ src/app/goal/goal.page.ts
@@ -16,6 +16,8 @@ export class GoalPage {
   readonly table_name:string = "pufflog"; // Table name
   tot_puffs_yesterday: number;
   total_day_puffs_yesterday: number;
+  stepdown: number;
+  plan_days: any=[];
 
   today = Date.now();
   lastLoggedDay: any;
@@ -39,35 +41,78 @@ export class GoalPage {
     })
       .then((db: SQLiteObject) => {
         this.databaseObj = db;
-   //     alert('Database' + this.database_name + ' Created!');
+     //   alert('Database' + this.database_name + ' Created!');
+        this.createTable();
 
+        this.getLastDayPuffs();
       })
       .catch(e => {
         alert('error ' + JSON.stringify(e));
       });
-
-      this.getLastDayPuffs();
+ 
   }
-
+  createTable() {
+    // tslint:disable-next-line:max-line-length
+    this.databaseObj.executeSql('CREATE TABLE IF NOT EXISTS ' + this.table_name + ' (pid INTEGER PRIMARY KEY, puffn INTEGER, created_at DEFAULT CURRENT_TIMESTAMP)', [])
+      .then(() => {
+   //     alert('Table Created!');
+      })
+      .catch(e => {
+        alert('error ' + JSON.stringify(e));
+      });
+  }
   getLastDayPuffs(){
-    alert("I call it");
-   // select the total puffs of day before
-   this.databaseObj.executeSql('SELECT SUM(puffn) AS \'somma\' FROM ' + this.table_name +' WHERE DATE(created_at) = DATEADD(day, -1, convert(date, GETDATE()))', [])
-   .then((res) => {
-    // alert(res.toSource());
-    if (res.rows.length > 0) {
-     for (let i = 0; i < res.rows.length; i++) {
-       // tslint:disable-next-line:forin
-       // tslint:disable-next-line:forin
-       this.tot_puffs_yesterday = res.rows.item(i).somma;
-     }
-   }
-    this.total_day_puffs_yesterday = this.tot_puffs_yesterday; 
-    alert(this.total_day_puffs_yesterday);
-   })
-   .catch(e => {
-     alert('error ' + JSON.stringify(e));
-   });
+//    this.databaseObj.executeSql('SELECT SUM(puffn) AS \'somma\' FROM ' + this.table_name +' WHERE DATE(created_at) = date(\'now\',\'-1\',\'day\')', [])
+this.databaseObj.executeSql('SELECT SUM(puffn) AS \'somma\' FROM ' + this.table_name +' WHERE DATE(created_at) = date(\'now\')', [])
+    .then((res) => {
+     // alert(res.toSource());
+     if (res.rows.length > 0) {
+      for (let i = 0; i < res.rows.length; i++) {
+        // tslint:disable-next-line:forin
+        // tslint:disable-next-line:forin
+        this.tot_puffs_yesterday = res.rows.item(i).somma;
+      }
+    }
+     this.total_day_puffs_yesterday = this.tot_puffs_yesterday;
+//     alert(this.total_day_puffs);
+    })
+    .catch(e => {
+      alert('error ' + JSON.stringify(e));
+    });
   }
+
+// quit in 10 days
+createPlan1(){
+  // divide the current daily puffs by 10 (on one day or more days and do average)
+  this.stepdown = this.total_day_puffs_yesterday/10;
+  var i=0;
+  var floatingPointPart = (this.total_day_puffs_yesterday/10) % 1;
+  var newtotalpuffs = this.total_day_puffs_yesterday;
+  if(floatingPointPart==0){
+    for(i=0;i<10;i++){
+     // alert("inside for");
+      this.plan_days[i]=newtotalpuffs - (this.stepdown);
+      newtotalpuffs= newtotalpuffs-(this.stepdown);
+    //  alert(this.plan_days[i]);
+    }
+  }
+
+
+
+var integerPart = Math.round(this.total_day_puffs_yesterday/10);
+
+//  alert(floatingPointPart);
+//  alert(integerPart);
+
+}
+
+// quit in 15 days
+createPlan2(){}
+
+// quit in 30 days
+createPlan3(){}
+
+//custom plan
+
 
 }
